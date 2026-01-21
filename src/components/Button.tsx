96622f60a4b64ef79c4e3a4235fa605216cb270f
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-21 19:50:46 +0530

    added a counter

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface MyButtonProps{
-    text: string,
-    onClick:()=>void,
-    onClick1?:()=>void
+    text: string, // required
+    onClick:()=>void, // required
+    onClick1?:()=>void // making optional
+    optional?: string | number | boolean
 }
 
+interface MyState{
+    state: number,
+}
 export const MyButton: React.FC <MyButtonProps>=(props)=>{
     // MyButton: React.FC for functional component
+
+    const {text, onClick} = props;
+    // destructuring the props
+
+
+    const [count, setCount] = useState<number>(0)
+
+    //  using state
+    
     return(
         <>
-        <button onClick={props.onClick}>{props.text}</button>
-        <button onClick={props.onClick1}>{props.text}</button>
-
+        <h1>{count}</h1>
+        {/* <button onClick={props.onClick}>{props.text}</button> */}
+        <button onClick={()=>setCount(count+1)}>{text}</button>
+        {/* <button onClick={props.onClick1}>{props.text}</button> */}
         </>
     )
 
