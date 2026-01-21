6371e4c46f0889e2c01f5cf65482fa1a96be136d
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-21 14:54:29 +0530

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Todo } from "./components/Todo";
 
 export const App: React.FC =()=>{
-  return <h1>Hello app</h1>
+  return (
+    <>
+    <Todo />
+    <Todo />
+    <Todo />
+    <Todo />
+    </>
+  )
 
 
 }
