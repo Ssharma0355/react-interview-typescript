e83e185b6d3b247a1d48370c451b8b4f9876414f
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-23 09:10:19 +0530

    fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import { Todo } from "./components/Todo";
 import { MyButton } from "./components/Button";
 import { OpenForms } from "./components/OpenForms";
 import { Practice } from "./components/Todo/Practice";
+import { PersonInfo } from "./components/PersonInfo";
 
 export const App: React.FC =()=>{
   return (
     <>
-    <MyButton text={"Click me"}  onClick={()=>{console.log("button clicked")}} onClick1={()=>{console.log("Optional click")}}/>
+    {/* <MyButton text={"Click me"}  onClick={()=>{console.log("button clicked")}} onClick1={()=>{console.log("Optional click")}}/> */}
     {/* <Todo />
     <Todo />
     <Todo />
     <Todo /> */}
-    <OpenForms />
-    <Practice />
+    {/* <OpenForms />
+    <Practice /> */}
+    <PersonInfo />
     </>
   )
 
